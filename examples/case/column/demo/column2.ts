3da5deec968dac08ce861541e58af6dd876bfb5e
MUSHA314 <122444499+MUSHA314@users.noreply.github.com>
2023-01-11 18:29:04 +0800

    Update column2.ts

--- examples/case/column/demo/column2.ts
+++ examples/case/column/demo/column2.ts
@@ -1,15 +1,13 @@
 import { Chart } from '@antv/g2';
 
 const data = [
-  { type: '未知', value: 654, percent: 0.02 },
-  { type: '17 岁以下', value: 654, percent: 0.02 },
-  { type: '18-24 岁', value: 4400, percent: 0.2 },
-  { type: '25-29 岁', value: 5300, percent: 0.24 },
-  { type: '30-39 岁', value: 6200, percent: 0.28 },
-  { type: '40-49 岁', value: 3300, percent: 0.14 },
-  { type: '50 岁以上', value: 1500, percent: 0.06 },
+  { type: '18岁以下', value: 654, percent: 0.02 },
+  { type: '18-25 岁', value: 654, percent: 0.02 },
+  { type: '26-35 岁', value: 4400, percent: 0.2 },
+  { type: '36-45 岁', value: 5300, percent: 0.24 },
+  { type: '46-55 岁', value: 6200, percent: 0.28 },
+  { type: '56 岁', value: 3300, percent: 0.14 },
 ];
-
 const chart = new Chart({
   container: 'container',
   autoFit: true,
